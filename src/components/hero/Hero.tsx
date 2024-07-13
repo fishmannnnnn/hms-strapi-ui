a670c5d620e7bda23ab6c9d81b9f9c4a128fd1c3
Vlad <vlad.lemkov@yandex.ru>
2024-07-13 19:05:22 +0400

    chores: add grid layout

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,10 +1,33 @@
 import styles from './Hero.module.scss';
+import FilledButton from '../filledButton/FilledButton';
 const Hero = () => {
-  return (
+	return (
 		<div className={styles.container}>
-			Выставочная логистика с ХМС Экспо: Премиальный Сервис для Вашего
-			Успеха
+			<div className={styles.containerTop}>
+				<p>
+					<span>Выставочная логистика с ХМС Экспо</span>: Премиальный
+					Сервис для Вашего Успеха
+				</p>
+				<div className={styles.containerTopRight}>
+					<p>
+						Доставим ваш груз в любую точку мира на любое
+						мероприятие.
+					</p>
+					<FilledButton text='Доставить груз' />
+				</div>
+			</div>
+			<div className={styles.containerBottom}>
+				<div className={styles.block} id={styles.planeBlock}>
+                    <div className={styles.blockIcon} id={styles.circleWhite}></div>
+                </div>
+				<div className={styles.block} id={styles.truckBlock}>
+                    <div className={styles.blockIcon} id={styles.circleBlue}></div>
+                </div>
+				<div className={styles.block} id={styles.accessibilityBlock}>
+                    <div className={styles.blockIcon} id={styles.circleRed}></div>
+                </div>
+			</div>
 		</div>
-  );
-}
-export default Hero
+	);
+};
+export default Hero;
