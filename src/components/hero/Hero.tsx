591ca9062230456180d3c0a4318b75745235b070
Vlad <vlad.lemkov@yandex.ru>
2024-07-13 16:24:39 +0400

    chores: add hero section

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,10 @@
+import styles from './Hero.module.scss';
+const Hero = () => {
+  return (
+		<div className={styles.container}>
+			Выставочная логистика с ХМС Экспо: Премиальный Сервис для Вашего
+			Успеха
+		</div>
+  );
+}
+export default Hero
