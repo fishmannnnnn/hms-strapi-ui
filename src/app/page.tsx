f83acb168d7a0eed27dd05db18c727ba3ed85e9c
Vlad <vlad.lemkov@yandex.ru>
2024-07-16 17:44:30 +0400

    chores: add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import styles from './page.module.scss';
 import Hero from '@/components/hero/Hero';
+import About from '@/components/about/About';
+import Advantages from '@/components/advantages/Advantages';
 const Home = () => {
 	return (
 		<div className={styles.page}>
 			<Hero />
+			<About />
+			<Advantages />
 		</div>
 	);
 };
