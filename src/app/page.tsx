6be38f3316ee699024915d93f9ffb78d0e4b86ce
Vlad <vlad.lemkov@yandex.ru>
2024-07-18 17:09:11 +0400

    chores: add section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from '@/components/hero/Hero';
 import About from '@/components/about/About';
 import Advantages from '@/components/advantages/Advantages';
 import Stats from '@/components/stats/Stats';
+import Clients from '@/components/clients/Clients';
 const Home = () => {
 	return (
 		<div className={styles.page}>
@@ -10,6 +11,7 @@ const Home = () => {
 			<About />
 			<Advantages />
 			<Stats />
+			<Clients />
 		</div>
 	);
 };
